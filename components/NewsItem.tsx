d164a60d04fadb122d8e1203186f5052abdba77f
Chukwuka Emi <emichukwuka@gmail.com>
2021-09-18 13:06:35 +0100

    first commit

--- components/NewsItem.tsx
+++ components/NewsItem.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Link from "next/link";
-import moment from "moment";
 
 interface Props {
   id: number;
